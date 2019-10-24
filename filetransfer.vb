c9aac5acde81c00ec579c73e2920d8fe16b63593
SamuelSYLee <47802461+SamuelSYLee@users.noreply.github.com>
2019-10-24 15:22:08 +0800

    filetransfer.vb: Modify file selection method to GUI

--- filetransfer.vb
+++ filetransfer.vb
@@ -1,44 +1,44 @@
 Sub txtToxls()
-    On Error GoTo ErrorHandler 
-    Dim FileName As String, PathName  As String
+    Dim fd As FileDialog
+    Set fd = Application.FileDialog(msoFileDialogFilePicker)
     Dim cnt As Integer, i As Integer
 
-    PathName = Application.ActiveWorkbook.Path
+    'Set Application.FileDialog GUI
+    With fd
+        .AllowMultiSelect = False
+        .Title = "Please choose the log file for analysis."
+        .InitialFileName = Application.ActiveWorkbook.Path
+        .Filters.Clear
+        .Filters.Add ".txt file", "*.txt"
+    End With
     OutputFileNum = FreeFile
     cnt = 1
 
-    FileName = Inputbox("Please type in the .txt file name to transfer to .xls file.")
+    If fd.Show = -1 Then
+        MsgBox "Processing... " & vbCrlf & fd.SelectedItems(1)
+        
+        'Transferring .txt file to .xls file
+        Open fd.SelectedItems(1) For Input As #OutputFileNum
+        Do Until EOF(OutputFileNum)
+            Line Input #OutputFileNum, LineFromFile
+            Cells(cnt, 1).Value = LineFromFile
+            cnt = cnt + 1
+        Loop
+        Close #OutputFileNum
 
-    Open PathName & "\" & FileName & ".txt" For Input As #OutputFileNum
-    Msgbox "Processing..." & vbCrlf & PathName & "\" & FileName & ".txt"
+        'Delete the .txt file description
+        For i = 1 To 3 Step 1
+            Rows(1).Delete
+        Next
 
-    Do Until EOF(OutputFileNum)
-        Line Input #OutputFileNum, LineFromFile
-        Cells(cnt, 1).Value = LineFromFile
-        cnt = cnt + 1
-    Loop
+        'Select all data
+        Range("A1").Select
+        Range(Selection, Selection.End(xlDown)).Select
+        'Seperate each data by semicolon
+        Selection.TextToColumns Semicolon := True
 
-    Close #OutputFileNum
-
-    'Delete the .txt file description
-    For i = 1 To 3 Step 1
-        Rows(1).Delete
-    Next
-
-    'Select all data
-    Range("A1").Select
-    Range(Selection, Selection.End(xlDown)).Select
-    'Seperate each data by semicolon
-    Selection.TextToColumns Semicolon := True
-
-    Msgbox FileName & ".txt has already been transfered!"
-
-    Exit Sub
-
-    'Error resolve for the file not found in the path
-ErrorHandler:
-        MsgBox "Error " & Err.Number & "：" & Err.Description & vbCrlf & _
-        "There is no " & FileName & ".txt in the path."
-        FileName = Inputbox("Please type in the .txt file name to transfer to .xls file.")
-    Resume  
+        Msgbox fd.SelectedItems(1) & vbCrlf & "has already been transfered!"
+    Else
+        MsgBox "There is no .txt file under selection!"
+    End If
 End Sub
